f0ec43c586d9227995cb6155d6abfe4851204a67
Devon Jue <devon.jue@blackswan.com>
2016-06-28 01:39:16 -0700

    added encrypt decrypt problem

---
+++ week7/caesar_encrypt_decrypt.py
@@ -0,0 +1,278 @@
+import string
+
+### DO NOT MODIFY THIS FUNCTION ###
+def load_words(file_name):
+    '''
+    file_name (string): the name of the file containing 
+    the list of words to load    
+    
+    Returns: a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    '''
+    print 'Loading word list from file...'
+    # inFile: file
+    in_file = open(file_name, 'r', 0)
+    # line: string
+    line = in_file.readline()
+    # word_list: list of strings
+    word_list = line.split()
+    print '  ', len(word_list), 'words loaded.'
+    in_file.close()
+    return word_list
+
+### DO NOT MODIFY THIS FUNCTION ###
+def is_word(word_list, word):
+    '''
+    Determines if word is a valid word, ignoring
+    capitalization and punctuation
+
+    word_list (list): list of words in the dictionary.
+    word (string): a possible word.
+    
+    Returns: True if word is in word_list, False otherwise
+
+    Example:
+    >>> is_word(word_list, 'bat') returns
+    True
+    >>> is_word(word_list, 'asdf') returns
+    False
+    '''
+    word = word.lower()
+    word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
+    return word in word_list
+
+### DO NOT MODIFY THIS FUNCTION ###
+def get_story_string():
+    """
+    Returns: a joke in encrypted text.
+    """
+    f = open("story.txt", "r")
+    story = str(f.read())
+    f.close()
+    return story
+
+WORDLIST_FILENAME = 'words.txt'
+
+class Message(object):
+    ### DO NOT MODIFY THIS METHOD ###
+    def __init__(self, text):
+        '''
+        Initializes a Message object
+                
+        text (string): the message's text
+
+        a Message object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words
+        '''
+        self.message_text = text
+        self.valid_words = load_words(WORDLIST_FILENAME)
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_message_text(self):
+        '''
+        Used to safely access self.message_text outside of the class
+        
+        Returns: self.message_text
+        '''
+        return self.message_text
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_valid_words(self):
+        '''
+        Used to safely access a copy of self.valid_words outside of the class
+        
+        Returns: a COPY of self.valid_words
+        '''
+        return self.valid_words[:]
+        
+    def build_shift_dict(self, shift):
+        '''
+        Creates a dictionary that can be used to apply a cipher to a letter.
+        The dictionary maps every uppercase and lowercase letter to a
+        character shifted down the alphabet by the input shift. The dictionary
+        should have 52 keys of all the uppercase letters and all the lowercase
+        letters only.        
+        
+        shift (integer): the amount by which to shift every letter of the 
+        alphabet. 0 <= shift < 26
+
+        Returns: a dictionary mapping a letter (string) to 
+                 another letter (string). 
+        '''
+        shift_dict = {}
+
+        for letter in string.ascii_lowercase:
+            difference = (string.ascii_lowercase.index(letter) + shift) - 25
+            if difference > 0:
+                shift_dict[letter] = string.ascii_lowercase[difference - 1]
+            else:
+                shift_dict[letter] = string.ascii_lowercase[string.ascii_lowercase.index(letter) + shift]
+        
+        for letter in string.ascii_uppercase:
+            difference = (string.ascii_uppercase.index(letter) + shift) - 25
+            if difference > 0:
+                shift_dict[letter] = string.ascii_uppercase[difference - 1]
+            else:
+                shift_dict[letter] = string.ascii_uppercase[string.ascii_uppercase.index(letter) + shift]
+                
+        return shift_dict        
+                
+
+    def apply_shift(self, shift):
+        '''
+        Applies the Caesar Cipher to self.message_text with the input shift.
+        Creates a new string that is self.message_text shifted down the
+        alphabet by some number of characters determined by the input shift        
+        
+        shift (integer): the shift with which to encrypt the message.
+        0 <= shift < 26
+
+        Returns: the message text (string) in which every character is shifted
+             down the alphabet by the input shift
+        '''
+        shifted_dict = self.build_shift_dict(shift)
+        shifted_word = ""
+        
+        for character in self.message_text:
+            if character in shifted_dict:
+                shifted_word += shifted_dict[character]
+            else: 
+                shifted_word += character
+        
+        return shifted_word
+
+class PlaintextMessage(Message):
+    def __init__(self, text, shift):
+        '''
+        Initializes a PlaintextMessage object        
+        
+        text (string): the message's text
+        shift (integer): the shift associated with this message
+
+        A PlaintextMessage object inherits from Message and has five attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+            self.shift (integer, determined by input shift)
+            self.encrypting_dict (dictionary, built using shift)
+            self.message_text_encrypted (string, created using shift)
+
+        Hint: consider using the parent class constructor so less 
+        code is repeated
+        '''
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
+
+    def get_shift(self):
+        '''
+        Used to safely access self.shift outside of the class
+        
+        Returns: self.shift
+        '''
+        return self.shift
+
+    def get_encrypting_dict(self):
+        '''
+        Used to safely access a copy self.encrypting_dict outside of the class
+        
+        Returns: a COPY of self.encrypting_dict
+        '''
+        return self.encrypting_dict.copy()
+
+    def get_message_text_encrypted(self):
+        '''
+        Used to safely access self.message_text_encrypted outside of the class
+        
+        Returns: self.message_text_encrypted
+        '''
+        return self.message_text_encrypted
+
+    def change_shift(self, shift):
+        '''
+        Changes self.shift of the PlaintextMessage and updates other 
+        attributes determined by shift (ie. self.encrypting_dict and 
+        message_text_encrypted).
+        
+        shift (integer): the new shift that should be associated with this message.
+        0 <= shift < 26
+
+        Returns: nothing
+        '''
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
+
+
+class CiphertextMessage(Message):
+    def __init__(self, text):
+        '''
+        Initializes a CiphertextMessage object
+                
+        text (string): the message's text
+
+        a CiphertextMessage object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+        '''
+        Message.__init__(self, text)
+
+    def decrypt_message(self):
+        '''
+        Decrypt self.message_text by trying every possible shift value
+        and find the "best" one. We will define "best" as the shift that
+        creates the maximum number of real words when we use apply_shift(shift)
+        on the message text. If s is the original shift value used to encrypt
+        the message, then we would expect 26 - s to be the best shift value 
+        for decrypting it.
+
+        Note: if multiple shifts are  equally good such that they all create 
+        the maximum number of you may choose any of those shifts (and their
+        corresponding decrypted messages) to return
+
+        Returns: a tuple of the best shift value used to decrypt the message
+        and the decrypted message text using that shift value
+        '''
+        shift_increment = 1
+        best_shift = [0] * 2
+        best_shift_count = 0
+        
+        while shift_increment < 26:
+            word_match_count = 0
+            encrypted_text_array = self.apply_shift(26 - shift_increment).split(' ')
+            for word in encrypted_text_array:
+                if is_word(self.get_valid_words(), word):
+                    word_match_count += 1
+            if word_match_count > best_shift_count:
+                best_shift[0] = 26 - shift_increment
+                best_shift[1] = ' '.join(encrypted_text_array)
+                best_shift_count = word_match_count
+                
+            shift_increment += 1
+            
+        return tuple(best_shift)
+    
+    def decrypt_story(self):
+        self.message_text = get_story_string()
+        return self.decrypt_message()
+                    
+# # Example test case (PlaintextMessage)
+# plaintext = PlaintextMessage('hello', 2)
+# print 'Testing plain text message...'
+# print 'Expected Output: jgnnq'
+# print 'Actual Output:', plaintext.get_message_text_encrypted()
+#     
+# # Example test case (CiphertextMessage)
+# ciphertext = CiphertextMessage('jgnnq')
+# print 'Testing cipher text message...'
+# print 'Expected Output:', (24, 'hello')
+# print 'Actual Output:', ciphertext.decrypt_message()
+# 
+# # Example test case decrypt_story
+# ciphertext = CiphertextMessage('jgnnq')
+# print 'Testing decrypt story...'
+# print 'Expected Output:', (16, 'Jack Florey is a mythical character created on the spur of a moment to help cover an insufficiently planned hack. He has been registered for classes at MIT twice before, but has reportedly never passed a class. It has been the tradition of the residents of East Campus to become Jack Florey for a few nights each year to educate incoming students in the ways, means, and ethics of hacking.')
+# print 'Actual Output:', ciphertext.decrypt_story()
